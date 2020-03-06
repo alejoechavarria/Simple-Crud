5a12a0570e7f5008efd4e3970e5b92be1bff857b
Alejo Echavarria <alejandroechavarria96@gmail.com>
2020-03-06 12:59:08 -0500

    Issue #1 Done

---
+++ src/main/java/co/com/sc/SimpleCrudApplication.java
@@ -0,0 +1,13 @@
+package co.com.sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCrudApplication.class, args);
+	}
+
+}
